cb842e93d0029e06a2fcb66ba0bde69a15050fea
oscarrep <oscar.bcn.95@gmail.com>
2024-12-17 13:38:14 +0100

    1-4 done

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var options = { headers: { 'Accept': 'application/json' } };
 var reportAcudits = []; //joke:'', score:n, date:''
+var currentJoke = null;
 function getJoke() {
     fetch('https://icanhazdadjoke.com/', options)
         .then(function (res) { return res.json(); })
-        .then(function (data) { printJoke(data); });
+        .then(function (data) {
+        printJoke(data);
+        currentJoke = { joke: data.joke, score: 0, date: '' };
+    });
+    console.log(reportAcudits);
 }
 function printJoke(txt) {
     var jokesContainer = document.querySelector('.jokes-container');
@@ -12,12 +17,61 @@ function printJoke(txt) {
         return;
     }
     var print = jokesContainer.querySelector('h5');
-    if (print)
+    var jokeObj = { joke: '', score: 0, date: '' };
+    if (print) {
         print.textContent = txt.joke;
+        jokeObj.joke = txt.joke;
+        reportAcudits.push(jokeObj);
+    }
     else {
         var h5 = document.createElement('h5');
         h5.textContent = txt.joke;
+        jokeObj.joke = txt.joke;
+        reportAcudits.push(jokeObj);
         jokesContainer.appendChild(h5);
     }
 }
+function giveScore(toGive) {
+    if (!currentJoke)
+        console.error('No joke available');
+    var jokeObj = reportAcudits.find(function (j) { return j.joke == currentJoke.joke; });
+    if (jokeObj) {
+        jokeObj.score = toGive;
+        jokeObj.date = new Date().toISOString();
+    }
+    else
+        console.error('Joke not found');
+    console.log(reportAcudits);
+}
+function getWeather() {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=41.3888&longitude=2.159&hourly=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1')
+        .then(function (res) { return res.json(); })
+        .then(function (data) { printWeather(data); console.log(data); });
+}
+function printWeather(txt) {
+    var tempContainer = document.querySelector('.temp-container');
+    var currentTime = new Date().getUTCHours();
+    var hourIndex = txt.hourly.time.findIndex(function (t) {
+        var foundTime = new Date(t).getUTCHours();
+        return foundTime === currentTime;
+    });
+    var temp = 0;
+    if (hourIndex !== -1)
+        temp = txt.hourly.temperature_2m[hourIndex];
+    else
+        console.error('No data available');
+    if (!tempContainer) {
+        console.error('tempContainer not found');
+        return;
+    }
+    var print = tempContainer.querySelector('h6');
+    if (print)
+        print.textContent = "Temperature: ".concat(temp, "\u00B0C");
+    else {
+        var h6 = document.createElement('h6');
+        h6.textContent = "Temperature: ".concat(temp, "\u00B0C");
+        tempContainer.appendChild(h6);
+    }
+}
 getJoke();
+getWeather();
--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 const options = { headers: { 'Accept': 'application/json' } }
 
-let reportAcudits = [] //joke:'', score:n, date:''
+let reportAcudits: { joke: string; score: number; date: string }[] = [] //joke:'', score:n, date:''
+let currentJoke: { joke: string; score: number; date: string } | null = null;
 
 function getJoke(): void {
     fetch('https://icanhazdadjoke.com/', options)
         .then((res) => res.json())
-        .then((data) => { printJoke(data) });
+        .then((data) => {
+            printJoke(data)
+            currentJoke = { joke: data.joke, score: 0, date: '' }
+        });
+    console.log(reportAcudits);
 }
 
 function printJoke(txt: { joke: string }): void {
@@ -16,14 +21,72 @@ function printJoke(txt: { joke: string }): void {
         return;
     }
     let print = jokesContainer.querySelector('h5');
+    let jokeObj = { joke: '', score: 0, date: '' }
 
-    if (print) print.textContent = txt.joke;
+    if (print) {
+        print.textContent = txt.joke;
+        jokeObj.joke = txt.joke;
+        reportAcudits.push(jokeObj);
+    }
     else {
         const h5 = document.createElement('h5');
         h5.textContent = txt.joke;
+        jokeObj.joke = txt.joke;
+        reportAcudits.push(jokeObj);
         jokesContainer.appendChild(h5);
     }
 }
 
+function giveScore(toGive: number): void {
+    if (!currentJoke) console.error('No joke available');
+
+    let jokeObj = reportAcudits.find(j => j.joke == currentJoke!.joke);
+
+    if (jokeObj) {
+        jokeObj.score = toGive;
+        jokeObj.date = new Date().toISOString();
+    }
+    else console.error('Joke not found');
+
+    console.log(reportAcudits);
+}
+
+function getWeather(): void {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=41.3888&longitude=2.159&hourly=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1')
+        .then((res) => res.json())
+        .then((data) => { printWeather(data); console.log(data) });
+}
+
+function printWeather(txt: { hourly: { time: string[]; temperature_2m: number[] } }): void {
+    let tempContainer = document.querySelector('.temp-container');
+
+    const currentTime = new Date().getUTCHours();
+    let hourIndex = txt.hourly.time.findIndex((t) => {
+        const foundTime = new Date(t).getUTCHours();
+        return foundTime === currentTime;
+    })
+
+    let temp = 0;
+
+    if (hourIndex !== -1) temp = txt.hourly.temperature_2m[hourIndex];
+    else console.error('No data available');
+
+    if (!tempContainer) {
+        console.error('tempContainer not found');
+        return;
+    }
+    let print = tempContainer.querySelector('h6');
+
+    if (print) print.textContent = `Temperature: ${temp}°C`;
+    else {
+        const h6 = document.createElement('h6');
+        h6.textContent = `Temperature: ${temp}°C`;
+        tempContainer.appendChild(h6);
+    }
+
+
+}
+
 getJoke();
+getWeather();
 
